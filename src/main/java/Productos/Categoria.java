8c4795167c22b8b33e8f5952f3bd2760570f3bb7
mendox2 <mendozi226@gmail.com>
2024-05-03 11:27:48 -0500

    clase categoria

---
+++ src/main/java/Productos/Categoria.java
@@ -0,0 +1,53 @@
+package Productos;
+
+import java.util.Scanner;
+
+public class Categoria {
+
+    Scanner sc = new Scanner(System.in);
+
+    //1. Crear atributos.
+    private int idCat;
+    private String nomCat;
+
+    //2. Crear el constructor.
+
+    public Categoria(){
+
+    }
+
+    public Categoria(int idCat, String nomCat) {
+        this.idCat = idCat;
+        this.nomCat = nomCat;
+    }
+
+    public int getIdCat() {
+        return idCat;
+    }
+
+    public void setIdCat(int idCat) {
+        this.idCat = idCat;
+    }
+
+    public String getNomCat() {
+        return nomCat;
+    }
+
+    public void setNomCat(String nomCat) {
+        this.nomCat = nomCat;
+    }
+    //4. Crear metodos propios.
+
+    public void crearCategoria(){
+        System.out.println("Id categoria: ");
+        idCat = sc.nextInt();
+        sc.nextLine(); //Siempre que se haga un nextInt() hacer abajo un nextLine()
+        System.out.println("Nombre categoria: ");
+        nomCat = sc.nextLine();
+    }
+
+    public void seleccionarCategoria(){
+        System.out.println("Id categoria: "+ idCat);
+        System.out.println("Nombre categoria: "+ nomCat);
+    }
+}
